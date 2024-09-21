167b3769b3d0fa45cedbdcfea6fdfce5a4f9fe87
Sheila11-git <campillasheilamae@gmail.com>
2024-09-21 19:54:20 +0800

    Update and rename LA1_YAON_CS2D to Activity1.js

---
+++ NewFolder/Activity1.js
@@ -0,0 +1,9 @@
+let name = "Sheila Mae";
+let whatDoYouWannaBecomeInYourLife = "IWantToBecomeASuccessfulProgrammer";
+let gender = "Female";
+let facebookAccount = "Sheila Mae Yaon";
+
+console.log("Name", name);
+console.log("whatDoYouWannaBecomeInYourLife", whatDoYouWannaBecomeInYourLife);
+console.log("Gender", gender);
+console.log("FacebookAccount", facebookAccount);
