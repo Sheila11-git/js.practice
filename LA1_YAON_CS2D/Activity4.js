efc9f04c951e700b8b3e400399e6c1f0c7ebb8a6
Sheila11-git <campillasheilamae@gmail.com>
2024-09-21 21:38:57 +0800

    Create Activity4.js

---
+++ LA1_YAON_CS2D/Activity4.js
@@ -0,0 +1,13 @@
+// Create a variable named firstFavNum
+let firstFavNum = 26;
+// Creaye a variable named secondFavNum
+let secondFavNum = 2;
+
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum != secondFavNum);
