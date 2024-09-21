31f57fd52843ef7529ead1ba32ec6db9531e40b8
Sheila11-git <campillasheilamae@gmail.com>
2024-09-21 20:39:11 +0800

    Create Activity2.js

---
+++ LA1_YAON_CS2D/Activity2.js
@@ -0,0 +1,27 @@
+// Create a Variable
+let firstFavNum = 11; // store your first favorite number
+let secondFavNum = 26; // store your second favorite number
+
+// Addition
+let additionResult = firstFavNum + secondFavNum;
+console.log('Addition:', additionResult);
+
+// Subtraction
+let subtractionResult = firstFavNum - secondFavNum;
+console.log('Subtraction:', subtractionResult);
+
+// Multiplication
+let multiplicationResult = firstFavNum * secondFavNum;
+console.log('Multiplication:', multiplicationResult);
+
+// Division
+let divisionResult = firstFavNum / secondFavNum;
+console.log('Division:', divisionResult);
+
+// Modulus (Remainder)
+let modulusResult = firstFavNum % secondFavNum;
+console.log('Modulus:', modulusResult);
+
+// Exponentation (Power)
+let exponentationResult = firstFavNum ** secondFavNum;
+console.log('Exponentation', exponentationResult);
