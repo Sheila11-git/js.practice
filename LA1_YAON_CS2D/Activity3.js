e171e94bbfe66f9cd5b16233682cf0661766a9c3
Sheila11-git <campillasheilamae@gmail.com>
2024-09-21 21:20:18 +0800

    Create Activity3.js

---
+++ LA1_YAON_CS2D/Activity3.js
@@ -0,0 +1,17 @@
+// Step 1: Create a variable named isJsProgrammingLanguage and store true
+let isJsProgrammingLanguage = true; 
+
+// Step 2: Create a variable named isJsHard and store false
+let isJsHard = false;  
+
+// Step 3: Create a variable named favNumb
+let favNumb = 7;  // Store 7 as your favorite number
+
+// Step 4: Add favNumb with the value of undefined
+let sumWithUndefined = favNumb + undefined;  
+
+// Step 5: Log all the variables to a console
+console.log("isJsProgrammingLanguage:", isJsProgrammingLanguage);
+console.log("isJsHard:", isJsHard);
+console.log("favNumb:", favNumb);
+console.log("sumWithUndefined:", sumWithUndefined);
