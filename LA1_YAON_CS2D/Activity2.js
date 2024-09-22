19b0da713c9be03e987e5bbc447c77f162f9829f
Sheila11-git <campillasheilamae@gmail.com>
2024-09-22 15:47:07 +0800

    Update Activity2.js

--- LA1_YAON_CS2D/Activity2.js
+++ LA1_YAON_CS2D/Activity2.js
@@ -3,18 +3,22 @@ let firstFavNum = 11; // store your first favorite number
 let secondFavNum = 26; // store your second favorite number
 
 // Addition
+// Add (firstFavNum & secondFavNum)
 let additionResult = firstFavNum + secondFavNum;
 console.log('Addition:', additionResult);
 
 // Subtraction
+// Subtract (firstFavNum & secondFavNum)
 let subtractionResult = firstFavNum - secondFavNum;
 console.log('Subtraction:', subtractionResult);
 
 // Multiplication
+// Multiply (firstFavNum & secondFavNum)
 let multiplicationResult = firstFavNum * secondFavNum;
 console.log('Multiplication:', multiplicationResult);
 
 // Division
+// Divide (firstFavNum & secondFavNum)
 let divisionResult = firstFavNum / secondFavNum;
 console.log('Division:', divisionResult);
 
@@ -23,5 +27,6 @@ let modulusResult = firstFavNum % secondFavNum;
 console.log('Modulus:', modulusResult);
 
 // Exponentation (Power)
+// Check the power of (firstFavNum) raise to secondFavNun
 let exponentationResult = firstFavNum ** secondFavNum;
 console.log('Exponentation', exponentationResult);
