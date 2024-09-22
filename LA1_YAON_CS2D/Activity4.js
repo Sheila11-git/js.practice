c6b4b3713e6b69a7ed196688de40e438075fea1e
Sheila11-git <campillasheilamae@gmail.com>
2024-09-22 15:39:22 +0800

    Update Activity4.js

--- LA1_YAON_CS2D/Activity4.js
+++ LA1_YAON_CS2D/Activity4.js
@@ -1,13 +1,21 @@
 // Create a variable named firstFavNum
-let firstFavNum = 26;
-// Creaye a variable named secondFavNum
-let secondFavNum = 2;
+let firstFavNum = 26 // store your first favorite number
+// Create a variable named secondFavNum
+let secondFavNum = 2; //replace your second favorite number
 
+//check (firstFavNun is greater than secondFavNun)
 console.log(firstFavNum > secondFavNum);
+//check (firstFavNum us less than secondFavNum )
 console.log(firstFavNum > secondFavNum);
+//check (firstFavNum is greater than & equal tobsecondFavNum)
 console.log(firstFavNum >= secondFavNum);
+//check (firstFavNum is less than & equal to secondFavNum)
 console.log(firstFavNum <= secondFavNum);
+//check (firstFavNum is equal to secondFavnun) using strict equality operator
 console.log(firstFavNum === secondFavNum);
+//check (firstFavNum is equal to secondFavNum) using loose equality operator
 console.log(firstFavNum == secondFavNum);
+//check (firstFavNum is not equal to secondFavNum) using strict non-equality operator
 console.log(firstFavNum !== secondFavNum);
+//check (firstFavNum is not equal to secondFavNum) using loose non-equality operator
 console.log(firstFavNum != secondFavNum);
