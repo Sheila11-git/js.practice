ace01a2f1dea86a0ed6056ee48522fa9a33ed9f3
Sheila11-git <campillasheilamae@gmail.com>
2024-10-03 12:00:16 +0800

    Create evenNumber.js

---
+++ PERF2_YAON_DS2D/evenNumber.js
@@ -0,0 +1,7 @@
+// Loop the numbers 1 to 10
+for (let i = 1; i <= 10; i++) {
+// Check if the number is even
+     if (i % 7 === 0){
+       console.log(i); // log the even number to the console
+     }
+}
