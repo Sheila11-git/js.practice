828a5b2dae65a99294a8fc7f1375fe33cf9b6819
Sheila11-git <campillasheilamae@gmail.com>
2024-10-31 07:16:45 +0800

    Create userProfile.js

---
+++ CS2D_MTExam_YAON/userProfile.js
@@ -0,0 +1,15 @@
+// Problem 1
+
+// Collect user information
+let firstName = "Sheila Mae";     // user's First Name
+let lastName = "Yaon";            // user's Last Name
+let age = 20;                     // user's Age
+let favoriteNumber = 7;           // user's Favorite Number
+let favoriteColor = "Black";      // user's Favorite Color 
+
+// log user information to the console 
+console.log("User Profile");
+console.log("FullName:", firstName, lastName);
+console.log("Age");
+console.log("Favorite Number;");
+console.log("favorite Color;");
