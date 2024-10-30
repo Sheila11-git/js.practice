a98101177fc340f136f159bcf02efc2f55d10f46
Sheila11-git <campillasheilamae@gmail.com>
2024-10-31 07:47:09 +0800

    Create classSubject.js

---
+++ CS2D_MTExam_YAON/classSubject.js
@@ -0,0 +1,9 @@
+// collect user informstion
+let subjectTitle = "Data Structure and Algorithm";     // subject title
+let classSchedule = "8:00 AM - 9:30 AM, MONDAY";       // class scheduleb(time, day)
+let classroom = "CL B";                                // classroom schedule
+let classInstructor = "Cliff Owen PAscua";             // name of the instructor
+let studentName = "Sheila Mae Yaon";                   // name of the student
+
+// display the collected information in to the console
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for this subject is ${classInstructor}.`);
