ff265113eace9c07f692c99220b1dbe39b0d8b29
Sheila11-git <campillasheilamae@gmail.com>
2024-10-31 08:06:33 +0800

    Create Problem 6: UserProfileObject.js

---
+++ CS2D_MTExam_YAON/Problem 6: UserProfileObject.js
@@ -0,0 +1,12 @@
+// problem 6
+
+// store user profile information
+let userProfile = {
+    fullName: "Sheila Mae Yaon",                  // User's full name
+    age: 20,                                     // User's age
+    favoriteNumber: 7,                          // User's favorite number
+    favoriteColors: ["Black", "Red", "Blue"]   // Array of user's favorite colors
+};
+
+// Log the entire user profile object to the console
+console.log(userProfile);
