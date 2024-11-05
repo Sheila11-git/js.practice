5afcb2d2fc7c637329b64b7e55c204305aa5a424
Sheila11-git <campillasheilamae@gmail.com>
2024-11-05 12:01:42 +0800

    Update classSubject.js

--- CS2D_MTExam_YAON/classSubject.js
+++ CS2D_MTExam_YAON/classSubject.js
@@ -1,4 +1,4 @@
-// collect user informstion
+// collect user information
 let subjectTitle = "Data Structure and Algorithm";     // subject title
 let classSchedule = "8:00 AM - 9:30 AM, MONDAY";       // class scheduleb(time, day)
 let classroom = "CL B";                                // classroom schedule
